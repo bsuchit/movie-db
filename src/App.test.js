b7b0f5dd6b41d789a0fe91979947aedbccc92465
Suchit Bande <suchit.bande@yahoo.com>
2023-06-19 19:22:15 +0530

    unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Sort By button', () => {
   render(<App />);
   const linkElement = screen.getByText(/Sort By/i);
   expect(linkElement).toBeInTheDocument();
